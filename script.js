2035e424780d2cf3c77ad32387e3b90e774f1cb8
lucas <lucas@seguroviagem.srv.br>
2023-02-14 09:15:44 -0300

    colocando borda nas imagens

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ addCarimbo = () => {
     let img = document.createElement('img');
     img.src = './carimbo.jpg';
     img.alt = 'Carimbo!';
+    img.style.border = '3px solid';
+    img.style.borderColor = randomColor();
     carimbo.appendChild(img);
+}
+
+randomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
 removeCarimbo = () => {
